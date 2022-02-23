c0d1e7cf03bdde6d26770b7b139dd78e7a76e696
Tuğçe Eraslan <tugceeraslan@emekkitap.com>
2022-02-23 16:47:07 +0300

    + View Components Klasörü içerisinde MenuViewComponent.cs oluşturdum. Bu sayede artık Controller a bağlı olmadan veritabanından(şuan burada yok ama olsaydı veritabanı işlerimi bu kısımda halledecektim) alarak kullanıcıya home/index2 ve home/about sayfalarında göstereceğim.

---
+++ CourseApp/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using CourseApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CourseApp.ViewComponents
+{
+    public class MenuViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var categories = new List<Category>()   /*Burda oluşturduğumuz categories modelini view e göndermemiz gerekiyor. */
+            {
+                new Category(){Name="Kategori 7"},
+                new Category(){Name="Kategori 8"},
+                new Category(){Name="Kategori 9"}
+            };
+
+            return View(categories);  /* Ekrana yazdığı bu alanadan gelen bilgiler. Çünkü artık Controller a bağlı oluyorum. 
+                                       * Http talebi içersinde bir işlem yapmam gerekmiyor. ViewComponent esnek bir yapısı olan ve MVC Core
+                                       * ile gelen yeni bir kavram. */
+        }
+    }
+}
